9c5bc2098227c6c87925601d0623a89fc3b2bba1
AlexandrMay <maysalexandr@gmail.com>
2018-04-02 16:08:46 +0300

    secondcommit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
 
      {
-            int a = 10, b = 2, c = 0;
+        /*    int a = 10, b = 2, c = 0;
             if (a<b)
             {
                 Console.WriteLine("a<b");
@@ -34,10 +34,60 @@ namespace ConsoleApplication1
             Console.WriteLine(aa);
             aa = (byte)(aa >> 1);
             Console.WriteLine(aa);
+            */
 
+            //Циклы
+            int counter = 0;
+            /* Label:
+             counter++;
+             Console.WriteLine("Counter = {0}", counter);
+             if (counter < 3) goto Label;
+             Console.WriteLine("End");
+             Console.ReadKey();  
 
+             while (counter < 3)
+             {
+                 counter++;
+                 Console.WriteLine("Counter {0}", counter);
+             }
+             Console.ReadKey(); 
 
+            const int maxAttempt = 5;
+            int attempt = 0;
+            String color = "red";
+
+            while (attempt < maxAttempt)
+            {
+                attempt++;
+                Console.WriteLine("Попытка № {0}", attempt);
+
+                String value = Console.ReadLine();
+                if (value == "exit")
+                {
+                    break;
+                }
+                if (value != color)
+                {
+                    continue;
+                }
+                Console.WriteLine("Угадали с {0} попытки", attempt);
+                break;
+            } */
+
+            do
+            {
+                counter++;
+                Console.WriteLine("Counter is {0}", counter);
+            }
+            while (counter < 3);
+            Console.WriteLine("Произведено {0} итераций", counter);
             Console.ReadKey();
+
+
+
+
+
+
 
 
         }
